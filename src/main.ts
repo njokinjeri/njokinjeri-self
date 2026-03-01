1e871081b33a8360f2d22a8689932534678965c5
njoki <njerinjoki22@gmail.com>
2026-03-01 08:45:18 +0300

    update toggle icons path

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,11 @@ const themeIcon = document.getElementById("theme-icon") as HTMLImageElement;
 
 const prefersDark = window.matchMedia("(prefers-color-scheme: dark").matches;
 document.documentElement.setAttribute("data-theme", prefersDark ? "dark" : "light");
-themeIcon.src = prefersDark ? "./src/assets/moon-icon.svg" : "./src/assets/sun-icon.svg";
+themeIcon.src = prefersDark ? "/moon-icon.svg" : "/sun-icon.svg";
 
 themeToggle.addEventListener("click", () => {
     const current = document.documentElement.getAttribute("data-theme");
     const next = current === "dark" ? "light" : "dark";
     document.documentElement.setAttribute("data-theme", next);
-    themeIcon.src = next === "dark" ? "./src/assets/moon-icon.svg" : "./src/assets/sun-icon.svg";
+    themeIcon.src = next === "dark" ? "/moon-icon.svg" : "/sun-icon.svg";
 })
