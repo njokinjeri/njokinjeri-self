5e6b1059845808abc9d902c3e9797a8cd31ab56f
njoki <njerinjoki22@gmail.com>
2026-03-01 08:58:30 +0300

    fix media query syntax & import icon paths

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,18 @@
 import './ui/clock.ts';
 import './ui/uptime.ts';
+import moonIcon from "./assets/moon-icon.svg";
+import sunIcon from "./assets/sun-icon.svg";
 
 const themeToggle = document.getElementById("theme-toggle") as HTMLButtonElement;
 const themeIcon = document.getElementById("theme-icon") as HTMLImageElement;
 
-const prefersDark = window.matchMedia("(prefers-color-scheme: dark").matches;
+const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
 document.documentElement.setAttribute("data-theme", prefersDark ? "dark" : "light");
-themeIcon.src = prefersDark ? "/moon-icon.svg" : "/sun-icon.svg";
+themeIcon.src = prefersDark ? moonIcon : sunIcon;
 
 themeToggle.addEventListener("click", () => {
     const current = document.documentElement.getAttribute("data-theme");
     const next = current === "dark" ? "light" : "dark";
     document.documentElement.setAttribute("data-theme", next);
-    themeIcon.src = next === "dark" ? "/moon-icon.svg" : "/sun-icon.svg";
+    themeIcon.src = next === "dark" ? moonIcon : sunIcon;
 })
